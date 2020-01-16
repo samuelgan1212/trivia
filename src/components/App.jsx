d5743b1d07381ae8bf41e131ace5c351b32370fb
Student <student@Students-MacBook-Air.local>
2020-01-15 18:33:01 -0800

    something

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Question from Question.jsx;
 import '../css/App.css';
 // import components
 
@@ -6,10 +7,9 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        Trivia!
-        
-      </div>
+      <Question/>
+      
+      
     );
   }
 }
